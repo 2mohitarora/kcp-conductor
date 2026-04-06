9898c98b5998fd7afc36b2f4227e358a6978e901
Mohit Arora <mohit.arora0@walmart.com>
2026-04-05 22:22:51 -0400

    Fixed registry scripts

--- cluster-1-script.sh
+++ cluster-1-script.sh
@@ -1,9 +1,9 @@
-for node in vcluster.cp.cluster-2 vcluster.node.cluster-2.worker-1; do
-  docker exec "$node" mkdir -p /etc/containerd/certs.d/registry-2:5000
-  docker exec "$node" sh -c 'cat > /etc/containerd/certs.d/registry-2:5000/hosts.toml << EOF
-server = "http://registry-2:5000"
+for node in vcluster.cp.cluster-1 vcluster.node.cluster-1.worker-1; do
+  docker exec "$node" mkdir -p /etc/containerd/certs.d/registry-1:5000
+  docker exec "$node" sh -c 'cat > /etc/containerd/certs.d/registry-1:5000/hosts.toml << EOF
+server = "http://registry-1:5000"
 
-[host."http://registry-2:5000"]
+[host."http://registry-1:5000"]
   capabilities = ["pull", "resolve"]
   skip_verify = true
 EOF'
